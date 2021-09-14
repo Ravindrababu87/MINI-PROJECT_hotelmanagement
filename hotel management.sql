2f101b2c34f90fff8946286be19b0197b63e4185
Akshathakmurthy <56667497+Akshathakmurthy@users.noreply.github.com>
2021-09-14 19:45:33 +0530

    Update hotel management.sql

--- hotel management.sql
+++ hotel management.sql
@@ -1,1 +1,36 @@
-CREATE TABLE course (cId nvarchar(50),  courseName nvarchar(50),  courseDuration nvarchar(50))
+CREATE TABLE hotel (hId nvarchar(50),  hotelName nvarchar(50),  hotelCity nvarchar(50))
+
+GO
+CREATE PROC usp_InsertData (@hId nvarchar(50), @hName nvarchar(50), @hCity nvarchar(50))
+AS
+BEGIN
+	BEGIN TRY
+		IF @hname IS NOT NULL and @hcity IS NOT NULL
+			BEGIN
+				INSERT INTO dbo.hotel(hId, hotelName, hotelCity) VALUES (@hId, @hName, @hCity)
+			END
+		ELSE
+			BEGIN
+				RETURN -1
+			END
+	END TRY
+	BEGIN CATCH
+		RETURN -99
+	END CATCH
+END
+GO 
+
+
+EXEC usp_InsertData @hId='H001', @hName='Hoteldelluna', @hCity = 'SouthKorea'
+EXEC usp_InsertData @hId='H007', @hName='MorningBliss', @hCity = 'Singapore'
+
+
+GO
+CREATE PROC usp_getData
+AS
+BEGIN
+SELECT * FROM dbo.hotel
+END
+GO
+
+EXEC usp_getData
